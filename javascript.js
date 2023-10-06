0c9e687f82e4fbb0457694c44f82650c41ada74e
Kevin <kevin.rodriguez678@gmail.com>
2023-10-06 18:19:25 -0400

    create playround function which determines the outcome of a single round using if and else if statements to return the outcome

--- javascript.js
+++ javascript.js
@@ -10,4 +10,28 @@ function getComputerChoice() {
   return randomChoice = computerChoice[randomIndex]
 }
 
-console.log(getComputerChoice());
+// console.log(getComputerChoice());
+
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === "rock" && computerSelection === "paper") {
+    return "You Lose! Paper beats Rock"
+  } else if (playerSelection === "rock" && computerSelection === "scissors") {
+    return "You Win! Rock beats Scissors"
+  } else if (playerSelection === "paper" && computerSelection === "scissors") {
+    return "You Lose! Scissors beats Paper"
+  } else if (playerSelection === "paper" && computerSelection === "rock") {
+    return "You Win! Paper beats Rock"
+  } else if (playerSelection === "scissors" && computerSelection === "rock") {
+    return "You Lose! Rock beats Scissors"
+  } else if (playerSelection === "scissors" && computerSelection === "paper") {
+    return "You Win! Scissors beats paper"
+  } else if (playerSelection === computerSelection) {
+    return "Its' a Tie, try again."
+  }
+}
+
+const playerSelection = "paper"
+
+const computerSelection = getComputerChoice().toLowerCase()
+
+console.log(playRound(playerSelection, computerSelection));
